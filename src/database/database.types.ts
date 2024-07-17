02b72d8f3adf2f8494076c71dc6d59bbeef2be8f
Daniel Elegbe <daniel.elegbe@hotmail.co.uk>
2024-07-17 22:35:23 +0100

    add api key and update posts

--- src/database/database.types.ts
+++ src/database/database.types.ts
@@ -4,11 +4,20 @@ export type Generated<T> = T extends ColumnType<infer S, infer I, infer U>
   : ColumnType<T, T | undefined, T>;
 export type Timestamp = ColumnType<Date, Date | string, Date | string>;
 
+export type ApiKey = {
+    id: Generated<number>;
+    key: string;
+    createdAt: Generated<Timestamp>;
+    updatedAt: Generated<Timestamp>;
+};
 export type Post = {
     id: Generated<number>;
     title: string;
     body: string;
+    createdAt: Generated<Timestamp>;
+    updatedAt: Generated<Timestamp>;
 };
 export type DB = {
+    api_keys: ApiKey;
     posts: Post;
 };
